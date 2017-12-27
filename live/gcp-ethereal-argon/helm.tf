6b0096024a7c173e20d632f0da0ee47b7c95270e
Ilya Sotkov <ilya@sotkov.com>
2017-12-27 07:43:09 +0200

    Create an example Helm release resource

--- live/gcp-ethereal-argon/helm.tf
+++ live/gcp-ethereal-argon/helm.tf
@@ -1,10 +1,13 @@
-/*
-
 provider "helm" {}
 
-resource "helm_repository" "example" {}
+resource "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
 
-resource "helm_release" "example" {}
-
-*/
-
+resource "helm_release" "ingress_controller" {
+  name = "my-ingress-controller"
+  repository = "${helm_repository.stable.metadata.0.name}"
+  chart = "nginx-ingress"
+  values = "${file("helm/releases/nginx-ingress.yaml")}"
+}
