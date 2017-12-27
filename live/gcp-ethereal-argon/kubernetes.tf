974919e44e27c89baddc6f2eacc69ca003f60247
Ilya Sotkov <ilya@sotkov.com>
2017-12-27 08:29:57 +0200

    Add a Kubernetes namespace to create
    This is to test if kubernetes provider is working as we'd expect it to

--- live/gcp-ethereal-argon/kubernetes.tf
+++ live/gcp-ethereal-argon/kubernetes.tf
@@ -1,8 +1,15 @@
-/*
-
 provider "kubernetes" {}
 
-resource "kubernetes_namespace" "example" {}
+resource "kubernetes_namespace" "example" {
+  metadata {
+    annotations {
+      name = "example-annotation"
+    }
 
-*/
+    labels {
+      mylabel = "label-value"
+    }
 
+    name = "terraform-example-namespace"
+  }
+}
