13f5919c94aeb20cfa2811963cf1321cb6d05935
Ilya Sotkov <ilya@sotkov.com>
2017-12-27 17:08:07 +0200

    Add jenkins helm release

---
+++ live/helm-releases/jenkins.tf
@@ -0,0 +1,7 @@
+resource "helm_release" "jenkins" {
+  name = "jenkins"
+  repository = "${helm_repository.stable.metadata.0.name}"
+  chart = "jenkins"
+  values = "${file("/exekube/live/helm-releases/jenkins.yaml")}"
+  depends_on = ["helm_release.kube_lego"]
+}
