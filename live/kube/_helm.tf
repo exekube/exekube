ab84a2ff226034ce3a0d57c2eb646db09dd01a94
Ilya Sotkov <ilya@sotkov.com>
2017-12-29 06:01:53 +0200

    Add incubator Helm chart repository

--- live/kube/_helm.tf
+++ live/kube/_helm.tf
@@ -5,3 +5,8 @@ resource "helm_repository" "stable" {
   name = "stable"
   url  = "https://kubernetes-charts.storage.googleapis.com"
 }
+
+resource "helm_repository" "incubator" {
+  name = "incubator"
+  url  = "https://kubernetes-charts-incubator.storage.googleapis.com/"
+}
