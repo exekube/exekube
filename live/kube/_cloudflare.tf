e45bc72ec8a9771ca339638820eaac8a0770a8a6
Ilya Sotkov <ilya@sotkov.com>
2017-12-30 03:11:34 +0200

    Fix a typo in cloudflare provider, reduce sleep time after kube-lego

--- live/kube/_cloudflare.tf
+++ live/kube/_cloudflare.tf
@@ -1,22 +1,23 @@
 provider "cloudflare" {
   email = "${var.cloudflare_email}"
-  toke = "${var.cloudflare_token}"
+  token = "${var.cloudflare_token}"
 }
 
 resource "cloudflare_record" "c6ns_pw" {
-  domain     = "${var.cloudflare_domain}"
-  name       = "*"
-  value      = "${data.kubernetes_service.ingress_controller.load_balancer_ingress.0.ip}"
-  type       = "A"
-  proxied    = false
-  priority   = 1
   depends_on = ["helm_release.ingress_controller"]
+
+  domain   = "${var.cloudflare_domain}"
+  name     = "*"
+  value    = "${data.kubernetes_service.ingress_controller.load_balancer_ingress.0.ip}"
+  type     = "A"
+  proxied  = false
+  priority = 1
 }
 
 data "kubernetes_service" "ingress_controller" {
+  depends_on = ["helm_release.ingress_controller"]
+
   metadata {
     name = "my-ingress-controller-nginx-ingress-controller"
   }
-
-  depends_on = ["helm_release.ingress_controller"]
 }
