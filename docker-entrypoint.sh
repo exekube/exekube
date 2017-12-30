ca710c200631927baddf3f7a66ff57d024984b19
Ilya Sotkov <ilya@sotkov.com>
2017-12-30 11:41:29 +0200

    Add "refresh" to docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" = 'apply' ] || [ "$1" = 'init' ] || [ "$1" = 'destroy' ] || [ "$1" = 'plan' ]; then
+if [ "$1" = 'apply' ] || [ "$1" = 'init' ] || [ "$1" = 'destroy' ] || [ "$1" = 'plan' ] || [ "$1" = 'refresh' ]; then
         if [ -z "$2" ]; then
                 cd $XK_WORKDIR && terraform $1
         else
