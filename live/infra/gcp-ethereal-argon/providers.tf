5d2a64199fac96a3b65a744941e753eeba20c3e7
Ilya Sotkov <ilya@sotkov.com>
2017-12-31 07:33:20 +0200

    Fix gcp project region

--- live/infra/gcp-ethereal-argon/providers.tf
+++ live/infra/gcp-ethereal-argon/providers.tf
@@ -1,5 +1,5 @@
 provider "google" {
   credentials = "${file("/exekube/credentials.json")}"
   project     = "${var.gcp_project}"
-  region      = "${var.gcp_zone}"
+  region      = "${var.gcp_region}"
 }
--- live/infra/gcp-ethereal-argon/variables.tf
+++ live/infra/gcp-ethereal-argon/variables.tf
@@ -1,5 +1,9 @@
 variable "gcp_project" {}
 
+variable "gcp_region" {
+  default = "europe-west1"
+}
+
 variable "gcp_zone" {
   default = "europe-west1-d"
 }
