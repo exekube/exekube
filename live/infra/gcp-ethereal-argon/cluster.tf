2ac1e765224216c76f6ace19c5f532584786d85c
Ilya Sotkov <ilya@sotkov.com>
2018-01-02 05:29:19 +0200

    Simplify cluster and node pool names

--- live/infra/gcp-ethereal-argon/cluster.tf
+++ live/infra/gcp-ethereal-argon/cluster.tf
@@ -1,7 +1,7 @@
 module "gke_cluster" {
-  source             = "../../../modules/gke-cluster"
-  cluster_name       = "my-k8s-cluster"
-  nodepool_name      = "my-k8s-np"
+  source             = "../../../modules/infra/gke-cluster"
+  cluster_name       = "k8s-cluster"
+  nodepool_name      = "k8s-np"
   gke_version        = "1.8.4-gke.1"
   enable_legacy_auth = "true"
   gcp_project        = "${var.gcp_project}"
