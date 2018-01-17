f13a3f479edd35d907faef3bc3b898cc45b08179
Ilya Sotkov <ilya@sotkov.com>
2018-01-17 08:21:17 +0200

    Improve backup.sh, add time stamp to secret-${date}.yaml

--- backup.sh
+++ backup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 domain_zone="swarm.pw"
-source=("ci.${domain_zone}-tls" "wp.${domain_zone}-tls" "react.${domain_zone}-tls" "registry.${domain_zone}-tls" "charts.${domain_zone}-tls")
+source=("ci.x${domain_zone}-tls" "wp.${domain_zone}-tls" "react.${domain_zone}-tls" "registry.${domain_zone}-tls" "charts.${domain_zone}-tls")
+green="\e[1;32m"
+no_color="\e[0m"
+secret_file_name="secret-$(date '+%Y-%m-%d-%H-%M-%S').yaml"
 
 for i in ${source[@]}
 do
-  echo "---" >> /exekube/backup/tls/secret.yaml \
-  && kubectl get secret $i -o yaml >> /exekube/backup/tls/secret.yaml
+  echo "---" >> /exekube/backup/tls/secret-$(date '+%Y-%m-%d-%H-%M-%S').yaml \
+  && kubectl get secret $i -o yaml >> /exekube/backup/tls/${secret_file_name} \
+  && printf "${green}Secret '${i}' has been saved to ./backup/tls/${secret_file_name} successfully!\n${no_color}"
 done
