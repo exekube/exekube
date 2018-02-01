896681230053e180cd50272ca4006cf1f9bf6083
Ilya Sotkov <ilya@sotkov.com>
2018-02-01 09:44:08 +0200

    Fix bug with missing "echo"

--- charts/drone/push.sh
+++ charts/drone/push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 curl \
-        -u $($CHARTMUSEUM_USERNAME | base64 -d):$($CHARTMUSEUM_PASSWORD | base64 -d) \
+        -u $(echo $CHARTMUSEUM_USERNAME | base64 -d):$(echo $CHARTMUSEUM_PASSWORD | base64 -d) \
         --data-binary "@drone-0.3.0.tgz" \
         $CHARTMUSEUM_URL/api/charts
