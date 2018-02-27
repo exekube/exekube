1c5b3290f3c67955baa229b51217360a08496d55
Ilya Sotkov <ilya@sotkov.com>
2018-02-27 10:45:51 +0200

    Add *.rails.swarm.pw to backup.sh script (LE TLS backup)

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 domain_zone="swarm.pw"
-source=("staging.${domain_zone}-tls" "production.${domain_zone}-tls" "ci.${domain_zone}-tls" "wp.${domain_zone}-tls" "react.${domain_zone}-tls" "registry.${domain_zone}-tls" "charts.${domain_zone}-tls")
+source=("production.swarm.${domain_zone}-tls" "staging.rails.${domain_zone}-tls" "staging.${domain_zone}-tls" "production.${domain_zone}-tls" "ci.${domain_zone}-tls" "wp.${domain_zone}-tls" "react.${domain_zone}-tls" "registry.${domain_zone}-tls" "charts.${domain_zone}-tls" "drone.${domain_zone}-tls")
 green="\e[1;32m"
 no_color="\e[0m"
 secret_file_name="secret-$(date '+%Y-%m-%d-%H-%M-%S').yaml"
