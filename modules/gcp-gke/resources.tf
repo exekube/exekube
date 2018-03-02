3df8a18a4218061469e3eec3c6f646de2ff45156
Ilya Sotkov <ilya@sotkov.com>
2018-03-02 10:53:05 +0200

    Add logging and monitoring oauth_scopes for nodes

--- modules/gcp-gke/resources.tf
+++ modules/gcp-gke/resources.tf
@@ -30,6 +30,8 @@ resource "google_container_cluster" "gke_cluster" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
     ]
 
     machine_type = "${var.node_type}"
@@ -80,6 +82,8 @@ resource "google_container_node_pool" "nodepool" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
       "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
     ]
   }
 
