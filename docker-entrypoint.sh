ba12d8032a895b55278a66d29e2af25612df69ba
Ilya Sotkov <ilya@sotkov.com>
2018-03-10 15:12:05 +0200

    Make `xk` output cleaner

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = 'apply' ] || [ "$1" = 'destroy' ] || [ "$1" = 'plan' ] || [ "$1" = 'output' ]; then
         if [ -z "$2" ]; then
-                cd $TF_VAR_xk_live_dir && terragrunt $1-all
+                cd $TF_VAR_xk_live_dir && terragrunt $1-all 2>&1 | grep -v 'terragrunt'
         else
                 cd $2 && terragrunt $1-all
         fi
@@ -11,14 +11,14 @@ elif [ "$1" = 'up' ]; then
         if [ -z "$2" ]; then
                 cd $TF_VAR_xk_live_dir && terragrunt apply-all
         else
-                cd $2 && terragrunt apply-all
+                cd $2 && terragrunt apply-all 2>&1 | grep -v 'terragrunt'
         fi
 
 elif [ "$1" = 'down' ]; then
         if [ -z "$2" ]; then
                 cd $TF_VAR_xk_live_dir && terragrunt destroy-all
         else
-                cd $2 && terragrunt destroy-all
+                cd $2 && terragrunt destroy-all 2>&1 | grep -v 'terragrunt'
         fi
 
 elif [ "$1" = 'init' ]; then
