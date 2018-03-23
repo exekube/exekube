bfb9abfb4676f1c530901a66b0f2cdd2f7b27e74
Ilya Sotkov <ilya@sotkov.com>
2018-03-23 16:06:34 +0200

    Simplify the gcp-kms-secret-mgmt module, additional permissions in a tf file now

--- modules/gcp-kms-secret-mgmt/variables.tf
+++ modules/gcp-kms-secret-mgmt/variables.tf
@@ -1,46 +1,29 @@
 # ------------------------------------------------------------------------------
-# GOOGLE CLOUD PROJECT
+# REQUIRED VARIABLES
 # ------------------------------------------------------------------------------
 
 variable "project_id" {
-  description = "The id of the project we created via the gcp-project module"
+  description = "Project where resources will be created"
 }
 
-variable "terraform_credentials" {}
-variable "product_env" {}
+variable "serviceaccount_key" {
+  description = "Service account key for the project"
+}
 
 # ------------------------------------------------------------------------------
-# SECRETS MANAGEMENT AND KMS
+# OPTIONAL VARIABLES
 # ------------------------------------------------------------------------------
 
-variable "secret_store_location" {
+variable "keyring_name" {
+  default = "keyring"
+}
+
+variable "storage_location" {
   default = "europe-west1"
 }
 
-variable "keyring_admins" {
-  type        = "list"
-  description = "Users who have full controll over the keyring"
-  default     = []
-}
-
-variable "keyring_users" {
-  type        = "list"
-  description = "Users who can encrypt and decrypt keys in the keyring"
-  default     = []
-}
-
-variable "crypto_keys" {
-  type        = "list"
-  description = "A map for setting cryptographic keys"
-
-  # Format:
-  # crypto_key = "comma-separated crypto_key users"
-  # "team1" = "user:jon@example.com,user:anna@example.com,user:maria@example.com"
-  default = []
-}
-
-variable "crypto_keys_users" {
-  type = "list"
+variable "encryption_keys" {
+  description = "Names of encryption keys to create (a storage bucket will also be created for each)"
 
   default = []
 }
