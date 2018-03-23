3d6a404b3931b5d722b53e9822436f634aa43ca0
Ilya Sotkov <ilya@sotkov.com>
2018-03-23 16:07:05 +0200

    Remove project creation to the project-init script

--- modules/gcp-project/main.tf
+++ modules/gcp-project/main.tf
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# TERRAFORM REMOTE STATE BACKEND
+# TERRAFORM / PROVIDER CONFIG
 # ------------------------------------------------------------------------------
 
 terraform {
@@ -7,37 +7,18 @@ terraform {
   backend "gcs" {}
 }
 
-# ------------------------------------------------------------------------------
-# TERRAFORM PROVIDERS
-# ------------------------------------------------------------------------------
-
-provider "random" {}
-
 provider "google" {
-  credentials = "${var.terraform_credentials}"
+  project     = "${var.project_id}"
+  credentials = "${var.serviceaccount_key}"
 }
 
 # ------------------------------------------------------------------------------
 # GOOGLE CLOUD PROJECT
 # ------------------------------------------------------------------------------
 
-resource "random_id" "id" {
-  prefix      = "${var.product_env}-${var.product_name}-"
-  byte_length = 4
-}
-
-resource "google_project" "project" {
-  name            = "${random_id.id.hex}"
-  project_id      = "${random_id.id.hex}"
-  billing_account = "${var.billing_id}"
-  org_id          = "${var.organization_id}"
-  skip_delete     = false
-}
-
 resource "google_project_service" "services" {
   count = "${length(var.project_services)}"
 
-  project            = "${google_project.project.project_id}"
   disable_on_destroy = false
 
   service = "${element(var.project_services, count.index)}"
@@ -56,21 +37,18 @@ resource "google_project_service" "services" {
 # ------------------------------------------------------------------------------
 
 resource "google_compute_network" "network" {
-  project                 = "${google_project.project.project_id}"
-  name                    = "${var.product_env}-${var.product_name}-network"
+  name                    = "network"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnets" {
   count = "${length(var.cluster_subnets)}"
 
-  project = "${google_project.project.project_id}"
-
   name                     = "nodes"
   network                  = "${google_compute_network.network.self_link}"
   ip_cidr_range            = "${element(split(",", lookup(var.cluster_subnets, count.index)), 1)}"
-  private_ip_google_access = true
   region                   = "${element(split(",", lookup(var.cluster_subnets, count.index)), 0)}"
+  private_ip_google_access = true
 
   secondary_ip_range = [
     {
@@ -85,12 +63,10 @@ resource "google_compute_subnetwork" "subnets" {
 }
 
 resource "google_compute_firewall" "allow_nodes_internal" {
-  project = "${google_project.project.project_id}"
-
   name        = "allow-nodes-internal"
   description = "Allow traffic between nodes"
 
-  network  = "${google_compute_network.network.name}"
+  network  = "${google_compute_network.network.self_link}"
   priority = "65534"
 
   direction     = "INGRESS"
@@ -112,7 +88,6 @@ resource "google_compute_firewall" "allow_nodes_internal" {
 }
 
 resource "google_compute_firewall" "allow_pods_internal" {
-  project  = "${google_project.project.project_id}"
   name     = "allow-pods-internal"
   network  = "${google_compute_network.network.name}"
   priority = "1000"
@@ -143,33 +118,35 @@ resource "google_compute_firewall" "allow_pods_internal" {
 }
 
 # ------------------------------------------------------------------------------
-# DNS ZONES AND EXTERNAL IP ADDRESS
+# EXTERNAL IP ADDRESS
 # ------------------------------------------------------------------------------
 
 resource "google_compute_address" "ingress_controller_ip" {
-  count   = "${var.create_static_ip_address ? 1 : 0}"
-  project = "${google_project.project.project_id}"
+  count = "${var.create_static_ip_address ? 1 : 0}"
 
   name         = "ingress-controller-ip"
   region       = "europe-west1"
   address_type = "EXTERNAL"
 }
 
+# ------------------------------------------------------------------------------
+# DNS ZONES AND RECORDS
+# ------------------------------------------------------------------------------
+
 resource "google_dns_managed_zone" "dns_zones" {
-  count   = "${length(var.dns_zones) > 0 ? length(var.dns_zones) : 0}"
-  project = "${google_project.project.project_id}"
+  count = "${length(var.dns_zones) > 0 ? length(var.dns_zones) : 0}"
 
   name     = "${element(keys(var.dns_zones), count.index)}"
   dns_name = "${element(values(var.dns_zones), count.index)}"
 }
 
-resource "google_dns_record_set" "ingress_domains" {
-  count   = "${length(var.dns_zones) > 0 && length(var.ingress_domains) > 0 ? length(var.ingress_domains) : 0}"
-  project = "${google_project.project.project_id}"
-  type    = "A"
-  ttl     = 3600
+resource "google_dns_record_set" "dns_records" {
+  count = "${length(var.dns_zones) > 0 && length(var.dns_records) > 0 ? length(var.dns_records) : 0}"
 
-  managed_zone = "${element(keys(var.ingress_domains), count.index)}"
-  name         = "${element(values(var.ingress_domains), count.index)}"
+  type = "A"
+  ttl  = 3600
+
+  managed_zone = "${element(keys(var.dns_records), count.index)}"
+  name         = "${element(values(var.dns_records), count.index)}"
   rrdatas      = ["${google_compute_address.ingress_controller_ip.0.address}"]
 }
