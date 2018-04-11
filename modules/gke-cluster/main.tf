4ac6a8d6d7f1e3b0d8ca0ac6bb4b097005571636
Ilya Sotkov <ilya@sotkov.com>
2018-04-11 07:00:29 +0300

    Update gke-cluster module, add oath_scopes variable to it

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -46,7 +46,13 @@ resource "google_container_cluster" "cluster" {
     http_load_balancing {
       disabled = false
     }
+
+    network_policy_config {
+      disabled = false
+    }
   }
+
+  oauth_scopes = "${var.oauth_scopes}"
 
   node_config {
     machine_type = "${var.node_type}"
@@ -56,13 +62,6 @@ resource "google_container_cluster" "cluster" {
       project = "${var.project_id}"
       pool    = "default"
     }
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
   }
 
   network_policy {
--- modules/gke-cluster/variables.tf
+++ modules/gke-cluster/variables.tf
@@ -30,6 +30,17 @@ variable "additional_zones" {
   default = []
 }
 
+variable "oauth_scopes" {
+  type = "list"
+
+  default = [
+    "https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/devstorage.read_only",
+    "https://www.googleapis.com/auth/logging.write",
+    "https://www.googleapis.com/auth/monitoring",
+  ]
+}
+
 variable "node_type" {
   default = "n1-standard-2"
 }
