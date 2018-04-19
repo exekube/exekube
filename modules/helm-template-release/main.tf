1397f37c04e40f9754c5e9d96492a593571b76d0
Ilya Sotkov <ilya@sotkov.com>
2018-04-19 10:53:08 +0300

    Add "helm-template-release" module
    * This module allows to use Helm without Tiller
    
    * You can create, update, and destroy releases
    
    * Functionality is obviously limited
    
    * It uses `helm template ... | kubectl apply -f -` underneath

---
+++ modules/helm-template-release/main.tf
@@ -0,0 +1,99 @@
+provider "local" {}
+
+# ------------------------------------------------------------------------------
+# HELM RELEASE (TILLERLESS)
+# ------------------------------------------------------------------------------
+
+// Use `helm template` then `kubectl apply -f` to install a remote chart
+resource "null_resource" "helm_template_release" {
+  count      = "${var.disable_release ? 0 : 1}"
+  depends_on = ["null_resource.kubernetes_secrets"]
+
+  triggers {
+    values = "${data.template_file.release_values.rendered}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+helm fetch \
+--untar --version ${var.chart_version} \
+${var.chart_repo}/${var.chart_name} \
+&& helm template \
+--values ${local_file.interpolated_values.filename} \
+--name ${var.release_name} \
+--namespace ${var.release_namespace} \
+${path.root}/${var.chart_name} > ${path.root}/release.yaml \
+&& kubectl apply -f ${path.root}/release.yaml
+EOF
+  }
+}
+
+// We use a separate 'null_resource' so that update doesn't cause a destroy
+resource "null_resource" "helm_template_release_destroy" {
+  count = "${var.prevent_destroy ? 0 : 1}"
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete -f ${path.root}/release.yaml"
+  }
+}
+
+// Parsed (interpolated) YAML values file
+data "template_file" "release_values" {
+  template = "${file("${var.release_values}")}"
+
+  vars {
+    domain_name        = "${var.domain_name}"
+    load_balancer_ip   = "${var.load_balancer_ip}"
+    ingress_basic_auth = "${var.ingress_basic_auth["secret_name"]}"
+  }
+}
+
+resource "local_file" "interpolated_values" {
+  filename = "${path.root}/${var.release_values}"
+  content  = "${data.template_file.release_values.rendered}"
+}
+
+# ------------------------------------------------------------------------------
+# Create a Kubernetes secret before installing the chart
+# ------------------------------------------------------------------------------
+
+resource "null_resource" "kubernetes_secrets" {
+  count = "${var.disable_release ? 0 : length(var.kubernetes_secrets)}"
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f ${element(var.kubernetes_secrets, count.index)}"
+  }
+
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "kubectl delete -f ${element(var.kubernetes_secrets, count.index)}"
+  }
+}
+
+# ------------------------------------------------------------------------------
+# Create a Kubernetes secret for ingress basic authentication (htpasswd)
+# ------------------------------------------------------------------------------
+
+resource "null_resource" "ingress_basic_auth" {
+  count = "${var.disable_release || var.ingress_basic_auth["secret_name"] == "" ? 0 : 1}"
+
+  provisioner "local-exec" {
+    command = <<EOF
+kubectl --namespace ${var.release_namespace} \
+create secret generic \
+${var.ingress_basic_auth["secret_name"]} \
+--from-literal=auth=$(echo -n ${file("${var.ingress_basic_auth["password"]}")} \
+| htpasswd -i -n ${file("${var.ingress_basic_auth["username"]}")})
+EOF
+  }
+
+  provisioner "local-exec" {
+    when = "destroy"
+
+    command = <<EOF
+kubectl --namespace ${var.release_namespace} \
+delete secret ${var.ingress_basic_auth["secret_name"]}
+EOF
+  }
+}
---
+++ modules/helm-template-release/variables.tf
@@ -0,0 +1,66 @@
+# ------------------------------------------------------------------------------
+# REQUIRED VARIABLES
+# ------------------------------------------------------------------------------
+
+variable "release_name" {
+  description = "Name of your Helm release to create"
+}
+
+variable "chart_repo" {
+  description = "Name of chart repoisitory"
+}
+
+variable "chart_name" {
+  description = "Name or local path for chart to install"
+}
+
+# ------------------------------------------------------------------------------
+# OPTIONAL VARIABLES
+# ------------------------------------------------------------------------------
+
+variable "prevent_destroy" {
+  default = false
+}
+
+variable "disable_release" {
+  description = "Do not create the release"
+  default     = false
+}
+
+variable "release_namespace" {
+  description = "Namespace to install the release into"
+  default     = "default"
+}
+
+variable "chart_version" {
+  description = "Version of chart to install"
+  default     = ""
+}
+
+variable "release_values" {
+  description = "Specify values in a YAML file, relative to module's path"
+  default     = "values.yaml"
+}
+
+variable "kubernetes_secrets" {
+  description = "List paths to secrets to create before installing the chart"
+  default     = []
+}
+
+variable "domain_name" {
+  description = "Specify the domain name to use for ingress (interpolated in values.yaml)"
+  default     = ""
+}
+
+variable "load_balancer_ip" {
+  description = "Specify the IP of to use for LoadBalancer (used for ingress controllers)"
+  default     = ""
+}
+
+variable "ingress_basic_auth" {
+  default = {
+    secret_name = ""
+    username    = ""
+    password    = ""
+  }
+}
