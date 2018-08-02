da7b16092405b19c1c0c33b0ddd89389d3a81585
Sergey Nazarov <natarajaya@gmail.com>
2018-08-01 20:41:47 -0700

    Add missing configuration blocks into gcp-api-mgmt module

--- modules/gcp-api-mgmt/main.tf
+++ modules/gcp-api-mgmt/main.tf
@@ -1,3 +1,17 @@
+# ------------------------------------------------------------------------------
+# TERRAFORM / PROVIDER CONFIG
+# ------------------------------------------------------------------------------
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "gcs" {}
+}
+
+provider "google" {
+  project     = "${var.project_id}"
+  credentials = "${var.serviceaccount_key}"
+}
+
 # GCP sevices aka APIs to enable for the GCP project
 resource "google_project_service" "services" {
   count = "${length(var.project_services)}"
