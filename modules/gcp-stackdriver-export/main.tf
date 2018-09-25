60fdba71ef5c5986023614fbabe66678d0666a55
Tyler Roscoe <tyler@cryptio.net>
2018-09-25 12:52:40 -0600

    Add gcp-stackdriver-export.

---
+++ modules/gcp-stackdriver-export/main.tf
@@ -0,0 +1,54 @@
+# ------------------------------------------------------------------------------
+# TERRAFORM / PROVIDER CONFIG
+# ------------------------------------------------------------------------------
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "gcs" {}
+}
+
+provider "google" {
+  project     = "${var.project_id}"
+  credentials = "${var.serviceaccount_key}"
+}
+
+# ------------------------------------------------------------------------------
+# GOOGLE STACKDRIVER EXPORT
+# ------------------------------------------------------------------------------
+
+resource "google_storage_bucket" "exported-logs" {
+  name = "${var.project_id}-exported-logs"
+}
+
+resource "google_logging_project_sink" "my-export" {
+  count       = "${length(var.exports)}"
+  name        = "${element(keys(var.exports), count.index)}"
+  destination = "storage.googleapis.com/${google_storage_bucket.exported-logs.name}"
+
+  # logName is a common thing to filter on. Per
+  # https://cloud.google.com/logging/docs/view/advanced-filters#minimize_global_and_substring_searches,
+  # it is best to match on an exact string. Doing so requires knowing
+  # var.project_id (since logName starts with "projects/my-project-id/").
+  # Neither Terraform variables (var.project_id) nor their environment variable
+  # cousins (TF_VAR_project_id) are availble from terraform.tfvars, where an
+  # instance of this module declares its filters. Hence, this workaround to
+  # replace the string "__var.project_id__" with the value of ${var.project_id}
+  # at runtime.
+  filter = "${replace(
+              element(values(var.exports), count.index),
+              "__var.project_id__",
+              "${var.project_id}"
+            )}"
+
+  unique_writer_identity = true
+}
+
+# Because our sink uses a unique_writer, we must grant that writer access to
+# the bucket.
+resource "google_project_iam_binding" "exported-logs-writer" {
+  role = "roles/storage.objectCreator"
+
+  members = [
+    "${google_logging_project_sink.my-export.writer_identity}",
+  ]
+}
---
+++ modules/gcp-stackdriver-export/variables.tf
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------------------------
+# REQUIRED VARIABLES
+# ------------------------------------------------------------------------------
+
+variable "project_id" {
+  description = "Project where resources will be created"
+}
+
+variable "serviceaccount_key" {
+  description = "Service account key for the project"
+}
+
+# ------------------------------------------------------------------------------
+# OPTIONAL VARIABLES
+# ------------------------------------------------------------------------------
+
+variable "exports" {
+  type        = "map"
+  description = "Map of Stackdriver export rules where keys are names of rules and values are filters implementing those rules"
+
+  # Example: {"exclude-all-foo" = "logName=\"projects/__var.project_id__/logs/foo\"}
+  default = {}
+}
