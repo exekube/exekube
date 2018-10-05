e381fd717fe5451f9ccde829ff9b47461f5a888f
Tyler Roscoe <tyler@cryptio.net>
2018-10-04 21:11:32 -0600

    Fix example.

--- modules/gcp-stackdriver-export/variables.tf
+++ modules/gcp-stackdriver-export/variables.tf
@@ -18,7 +18,7 @@ variable "exports" {
   type        = "map"
   description = "Map of Stackdriver export rules where keys are names of rules and values are filters implementing those rules. (__var.project_id__ is replaced with $var.project_id at runtime -- useful for matching logName exactly for efficient filtering.)"
 
-  # Example: {"exclude-all-foo" = "logName=\"projects/__var.project_id__/logs/foo\"}
+  # Example: {"export-all-foo" = "logName=\"projects/__var.project_id__/logs/foo\"}
   default = {}
 }
 
