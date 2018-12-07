6bbd0951058d9d75ab28e3d4c535764f980f9023
Stepan Stipl <stepan@stipl.net>
2018-12-07 16:52:19 +0000

    Add support for regional clusters
    This is non-breaking change, if you use and want to keep using zonal
    cluster, no changes needed.
    
    Migration - If you already have existing cluster, you should set
    `region` argument, unset `main_compute_zone` one. Before running
    `terraform apply` move object in Terrafrom state to a new name, such as
    `terraform state mv google_container_cluster.cluster
    google_container_cluster.cluster-regional`. This will still re-create a
    cluster, but it should work without manual intervention.

--- modules/gke-cluster/main.tf
+++ modules/gke-cluster/main.tf
@@ -24,10 +24,17 @@ provider "google-beta" {
 # GOOGLE KUBERNTES ENGINE CLUSTER
 # ------------------------------------------------------------------------------
 
+# Check that only one of region and main_compute_zone is set
+resource "null_resource" "region_or_zone" {
+  count                                                         = "${var.region == "" || var.main_compute_zone == "" ? 0 : 1}"
+  "ERROR: Only one of region and main_compute_zone may be set." = true
+}
+
 resource "google_container_cluster" "cluster" {
-  name = "${var.cluster_name}"
-  zone = "${var.main_compute_zone}"
   provider = "google-beta"
+  count    = "${var.region == "" ? 1 : 0}"
+  name     = "${var.cluster_name}"
+  zone     = "${var.main_compute_zone}"
 
   additional_zones = "${var.additional_zones}"
 
@@ -105,5 +112,101 @@ creator-cluster-admin-binding \
 --user=$(gcloud info --format='value(config.account)') \
 && helm init --client-only
 EOF
+  }
+}
+
+resource "google_container_cluster" "cluster-regional" {
+  provider = "google-beta"
+  count    = "${var.region == "" ? 0 : 1}"
+  name     = "${var.cluster_name}"
+  region   = "${var.region}"
+
+  additional_zones = "${var.additional_zones}"
+
+  initial_node_count      = "${var.initial_node_count}"
+  node_version            = "${var.kubernetes_version}"
+  min_master_version      = "${var.kubernetes_version}"
+  enable_kubernetes_alpha = "${var.enable_kubernetes_alpha}"
+  enable_legacy_abac      = "false"
+  network                 = "${var.network_name}"
+  subnetwork              = "nodes"
+
+  ip_allocation_policy {
+    cluster_secondary_range_name  = "pods"
+    services_secondary_range_name = "services"
+  }
+
+  addons_config {
+    horizontal_pod_autoscaling {
+      disabled = false
+    }
+
+    kubernetes_dashboard {
+      disabled = "${var.dashboard_disabled}"
+    }
+
+    http_load_balancing {
+      disabled = false
+    }
+
+    network_policy_config {
+      disabled = false
+    }
+  }
+
+  monitoring_service = "${var.monitoring_service}"
+  logging_service    = "${var.logging_service}"
+
+  maintenance_policy {
+    daily_maintenance_window {
+      start_time = "03:00"
+    }
+  }
+
+  node_config {
+    machine_type = "${var.node_type}"
+    disk_size_gb = 200
+    oauth_scopes = "${var.oauth_scopes}"
+    image_type   = "${var.node_image_type}"
+
+    labels {
+      project = "${var.project_id}"
+      pool    = "default"
+    }
+  }
+
+  network_policy {
+    enabled  = true
+    provider = "CALICO"
+  }
+
+  master_auth {
+    username = "${var.master_auth_username}"
+    password = "${var.master_auth_password}"
+
+    client_certificate_config {
+      issue_client_certificate = "${var.issue_client_certificate}"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+gcloud auth activate-service-account --key-file ${var.serviceaccount_key} \
+&& gcloud container clusters get-credentials ${var.cluster_name} \
+--region ${var.region} \
+--project ${var.project_id} \
+&& kubectl label ns kube-system name=kube-system \
+&& kubectl create clusterrolebinding \
+creator-cluster-admin-binding \
+--clusterrole=cluster-admin \
+--user=$(gcloud info --format='value(config.account)') \
+&& helm init --client-only
+EOF
+  }
+
+  timeouts {
+    create = "${var.create_timeout}"
+    update = "${var.update_timeout}"
+    delete = "${var.delete_timeout}"
   }
 }
