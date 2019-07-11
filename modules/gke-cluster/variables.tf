1a4c46e1724d65eacd392e166f3c88268a9ece7f
Tyler Roscoe <tyler@cryptio.net>
2019-07-10 23:01:34 -0600

    Different strategy for passing patterns without list support.

--- modules/gke-cluster/variables.tf
+++ modules/gke-cluster/variables.tf
@@ -63,6 +63,50 @@ variable "node_image_type" {
   default = "COS"
 }
 
+variable "binary_authorization_evaluation_mode" {
+  default = "ALWAYS_ALLOW"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_0" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_1" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_2" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_3" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_4" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_5" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_6" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
+variable "binary_authorization_admission_whitelist_pattern_7" {
+  # This value cannot be empty, so use a placeholder value that satisfies Google's API.
+  default = "PLACE.HOLDER/PATTERN"
+}
+
 variable "initial_node_count" {
   default = 2
 }
