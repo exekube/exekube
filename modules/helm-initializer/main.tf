1202c233cc0584f9675d55e94a42043865e71198
Stepan Stipl <stepan@stipl.net>
2019-08-01 11:56:21 +0100

    GPII-4040: Make tiller_status fail fast when it can't reach the cluster

--- modules/helm-initializer/main.tf
+++ modules/helm-initializer/main.tf
@@ -44,7 +44,7 @@ data "external" "tiller_status" {
   program = [
     "bash",
     "-c",
-    "REPLICAS=$$(kubectl get deploy tiller-deploy -n ${var.tiller_namespace} -o jsonpath='{.status.readyReplicas}'); [ \"$$REPLICAS\" != \"1\" ] && REPLICAS=\"$$RANDOM\"; jq -n --arg replicas \"$$REPLICAS\" '{readyReplicas:$$replicas}'",
+    "REPLICAS=$$(kubectl get deploy tiller-deploy --request-timeout 5s -n ${var.tiller_namespace} -o jsonpath='{.status.readyReplicas}'); [ \"$$REPLICAS\" != \"1\" ] && REPLICAS=\"$$RANDOM\"; jq -n --arg replicas \"$$REPLICAS\" '{readyReplicas:$$replicas}'",
   ]
 }
 
